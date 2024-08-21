097597985e336c27577c8efc551a1ca11daa9b76
dariakurb <dariakurbatova@dawsoncollege.qc.ca>
2024-08-21 13:39:17 -0400

    add validation for property object

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import flask
 from property import Property
 app = flask.Flask(__name__)
 
-property = Property(1, "House", 30000, "123 simple street", 2, 2, 20000, 40000)
+property = Property(1, 'Land', 40000, "123 simple street", 2, 2, 20000, 40000)
 @app.route('/')
 def index():
     return f'{property}'
