add1e32c8c5a0e84b9b1191703610999e1368f81
dariakurb <dariakurbatova@dawsoncollege.qc.ca>
2024-08-21 13:54:35 -0400

    add route to show all properties

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 import flask
+from flask import abort
 from property import Property
 app = flask.Flask(__name__)
 
-property = Property(1, 'Land', 40000, "123 simple street", 2, 2, 20000, 40000)
+property1 = Property(1, 'Land', 40000, "123 simple street", 2, 2, 20000, 40000)
+property2 = Property(1, 'Land', 40000, "123 simple street", 2, 2, 20000, 40000)
+property3 = Property(1, 'Land', 40000, "123 simple street", 2, 2, 20000, 40000)
+
+properties = [property1, property2, property3]
+
 @app.route('/')
 def index():
-    return f'{property}'
+    return f'{property1}'
+
+@app.route('/properties/')
+def get_properties():
+    if not properties or len(properties)==0:
+        abort(404)
+    listProperties='<ul>'
+    for property in properties:
+        listProperties += f'<li>{property}</li>'
+    listProperties += '</ul>'
+    return listProperties
+
