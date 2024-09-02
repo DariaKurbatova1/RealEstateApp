bd6aa7b7c3a041ea89c94a3a6300c796aff086fd
DariaKurbatova02 <daria.kurbatova@dawsoncollege.qc.ca>
2024-09-02 13:42:28 -0400

    add some properties to display on main page

--- home_view.py
+++ home_view.py
@@ -1,8 +1,15 @@
 from flask import (Blueprint, render_template)
 from . import app
+from . property import Property
 
 #bp = Blueprint('home_view', __name__, url_prefix='/')
 
+property1 = Property(1, 'Land', 40000, "123 simple street", 2, 2, 20000, 40000)
+property2 = Property(1, 'Land', 40000, "123 simple street", 2, 2, 20000, 40000)
+property3 = Property(1, 'Land', 40000, "123 simple street", 2, 2, 20000)
+
+properties = [property1, property2, property3]
+
 @app.route("/")
 def index():
-  return render_template('index.html')
+  return render_template('index.html', properties=properties)
