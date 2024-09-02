6d02e42cbb2ce61319bb93d3394576ef713c52c5
DariaKurbatova02 <daria.kurbatova@dawsoncollege.qc.ca>
2024-09-02 17:51:50 -0400

    add functionality to add property to sell to database

--- flaskrealestateapp/sell_views.py
+++ flaskrealestateapp/sell_views.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, render_template, request)
+from flask import (Blueprint, render_template, request, redirect, url_for)
 from flaskrealestateapp.property import Property
 from flask import Flask
 from pymongo import MongoClient
@@ -16,7 +16,23 @@ bp = Blueprint('sell_view', __name__, url_prefix='/sell/')
 @bp.route("/", methods=['GET', 'POST'])
 def sell_property():
     if request.method == 'POST':
-        pass
+        type = request.form['type']
+        price = request.form['price']
+        address = request.form['address']
+        bedroomNum = request.form['bedroomNum']
+        bathroomNum = request.form['bathroomNum']
+        squareFeet = request.form['squareFeet']
+        lotSize = request.form['lotSize']
+        properties.insert_one({
+            'type': type,
+            'price': price,
+            'address': address,
+            'bedroomNum': bedroomNum,
+            'bathroomNum': bathroomNum,
+            'squareFeet': squareFeet,
+            'lotSize': lotSize
+        })
+        return redirect(url_for('home_view.index'))
 
 
     return render_template('sell.html')
