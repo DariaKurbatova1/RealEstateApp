60d6ca5237bf0a751cb54cb6690d9c6da94c0020
DariaKurbatova02 <daria.kurbatova@dawsoncollege.qc.ca>
2024-09-10 13:57:33 -0400

    get the files to upload from sell_views

--- flaskrealestateapp/sell_views.py
+++ flaskrealestateapp/sell_views.py
@@ -2,6 +2,8 @@ from flask import (Blueprint, render_template, request, redirect, url_for)
 from flaskrealestateapp.property import Property
 from flask import Flask
 from pymongo import MongoClient
+from werkzeug.utils import secure_filename
+import os
 
 #create db client
 client = MongoClient('localhost', 27017)
@@ -9,6 +11,12 @@ client = MongoClient('localhost', 27017)
 db = client.flask_properties
 #create collection
 properties = db.properties
+
+UPLOAD_FOLDER = './static/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 bp = Blueprint('sell_view', __name__, url_prefix='/sell/')
@@ -34,7 +42,29 @@ def sell_property():
             'squareFeet': squareFeet,
             'lotSize': lotSize
         })
+        #upload file
+        if 'file' not in request.files:
+            #flash('No file part')
+            print('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            #flash('No selected file')
+            print('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            basedir = os.path.abspath(os.path.dirname(__file__))
+            file.save(os.path.join(basedir, app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('home_view.index'))
         return redirect(url_for('home_view.index'))
 
 
     return render_template('sell.html')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
