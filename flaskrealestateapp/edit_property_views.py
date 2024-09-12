8ec75b62e363877bdebf1257e50302b3b2a264c7
DariaKurbatova02 <daria.kurbatova@dawsoncollege.qc.ca>
2024-09-12 16:38:43 -0400

    add filter and fix edit property image bug

--- flaskrealestateapp/edit_property_views.py
+++ flaskrealestateapp/edit_property_views.py
@@ -1,8 +1,10 @@
-from flask import (Blueprint, render_template, request)
+from flask import (Blueprint, render_template, request, redirect, url_for)
 # from . import app
 from flaskrealestateapp.property import Property
 from flask import Flask
 from pymongo import MongoClient
+from werkzeug.utils import secure_filename
+import os
 
 #create db client
 client = MongoClient('localhost', 27017)
@@ -22,7 +24,7 @@ bp = Blueprint('edit_property', __name__, url_prefix='/edit_property/')
 @bp.route("/<int:property_id>", methods=['GET', 'POST'])
 def edit_property(property_id):
     #select the edited document
-    property = properties.find_one({'id':1})
+    property = properties.find_one({'id':property_id})
     
     if request.method == 'POST':
         #validation
@@ -55,17 +57,11 @@ def edit_property(property_id):
             print('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            #flash('No selected file')
-            print('No selected file')
-            return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             basedir = os.path.abspath(os.path.dirname(__file__))
-            file.save(os.path.join(basedir, app.config['UPLOAD_FOLDER'], (property_id+'.jpg')))
-        
+            file.save(os.path.join(basedir, app.config['UPLOAD_FOLDER'], (str(property_id)+'.jpg')))
+        return redirect(url_for('home_view.index'))
         #delete the old instance of the record
     
     
@@ -73,3 +69,8 @@ def edit_property(property_id):
     
 
     return render_template('edit-property.html', property=property)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
