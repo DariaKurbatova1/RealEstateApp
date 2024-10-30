87559a98e42cf278ff4089ac21f71f38daf77ce1
DariaKurbatova02 <daria.kurbatova@dawsoncollege.qc.ca>
2024-10-30 12:06:34 -0400

    fix signup functionality

--- flaskrealestateapp/sign_up_views.py
+++ flaskrealestateapp/sign_up_views.py
@@ -26,11 +26,11 @@ def sign_up():
         # Validation checks
         try:
             if not email or '@' not in email:
-                flash("Invalid email. Please provide a valid email.")
+                flash("Invalid email. Please provide a valid email.", "error")
                 return render_template('signup.html')
             
             if len(password) < 6:
-                flash("Password should be at least 6 characters long.")
+                flash("Password should be at least 6 characters long.", "error")
                 return render_template('signup.html')
             
             if password != confirm_password:
@@ -39,27 +39,26 @@ def sign_up():
             
             #check if user already exists
             if users.find_one({'email': email}):
-                flash("User with this email already exists")
+                flash("User with this email already exists", "error")
                 return redirect(url_for('login_view.login'))
             
             #create user
             hashed_password = generate_password_hash(password)
             user = {
                 'email': email,
-                'password': hashed_password,
-                'name': name
+                'password': hashed_password
             }
             try:
                 users.insert_one(user)
-                flash("User created successfully. Please log in.")
+                flash("User created successfully. Please log in.", "success")
                 return redirect(url_for('login_view.login'))
             except Exception as e:
                     print(f"Error inserting user: {e}")
-                    flash("An error occurred while creating your account. Please try again.")
+                    flash("An error occurred while creating your account. Please try again.", "error")
                     return render_template('signup.html')
         except Exception as e:
             print(f"General error: {e}")
-            flash("An error occurred. Please try again.")
+            flash("An error occurred. Please try again.", "error")
             return render_template('signup.html')
     
     return render_template('signup.html')
