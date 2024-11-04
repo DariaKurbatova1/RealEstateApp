db8bf5aa674c58f1fa37c5c62c46938b00c8dc82
DariaKurbatova1 <kurbatova.daria@gmail.com>
2024-11-04 15:46:16 -0500

    fix buy and rent views

--- flaskrealestateapp/buy_views.py
+++ flaskrealestateapp/buy_views.py
@@ -3,6 +3,9 @@ from flask import Flask
 from pymongo import MongoClient
 from werkzeug.utils import secure_filename
 import os
+import gridfs
+import base64
+
 
 #create db client
 # client = MongoClient('localhost', 27017)
@@ -13,11 +16,17 @@ import os
 client = MongoClient(os.environ.get("MONGO_URI"))
 db = client.flask_properties
 properties = db.properties
+fs = gridfs.GridFS(db)
 
 bp = Blueprint('buy_view', __name__, url_prefix='/buy/')
 
 @bp.route("/", methods=['GET', 'POST'])
 def buy_property():
     query = {'offerType': 'sale'}
-    all_properties = properties.find(query)
+    all_properties = list(db.properties.find(query))
+    for property in all_properties:
+        image_file = fs.find_one({"property_id": property['id']})
+        if image_file:
+            image_data = base64.b64encode(image_file.read()).decode('utf-8')
+            property['image'] = image_data
     return render_template('buy.html', properties=all_properties)
--- flaskrealestateapp/rent_views.py
+++ flaskrealestateapp/rent_views.py
@@ -3,7 +3,8 @@ from flask import Flask
 from pymongo import MongoClient
 from werkzeug.utils import secure_filename
 import os
-
+import gridfs
+import base64
 #create db client
 # client = MongoClient('localhost', 27017)
 # #create mongodb database
@@ -13,11 +14,17 @@ import os
 client = MongoClient(os.environ.get("MONGO_URI"))
 db = client.flask_properties
 properties = db.properties
+fs = gridfs.GridFS(db)
 
 bp = Blueprint('rent_view', __name__, url_prefix='/rent/')
 
 @bp.route("/", methods=['GET', 'POST'])
 def rent_property():
     query = {'offerType': 'rent'}
-    all_properties = properties.find(query)
+    all_properties = list(db.properties.find(query))
+    for property in all_properties:
+        image_file = fs.find_one({"property_id": property['id']})
+        if image_file:
+            image_data = base64.b64encode(image_file.read()).decode('utf-8')
+            property['image'] = image_data
     return render_template('rent.html', properties=all_properties)
